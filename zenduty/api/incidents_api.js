95a8bd61696fb07b0237841fe7c0b735b530360b
AKASH KUMAR <33580834+freakash@users.noreply.github.com>
2019-07-11 18:59:07 +0530

    Update incidents_api.js

--- zenduty/api/incidents_api.js
+++ zenduty/api/incidents_api.js
@@ -8,37 +8,41 @@ class IncidentsApi{
 }
 getIncidents(payload)
 {
-    // payload=
-    //        {
-    //            'page':1,
-    //      'status':5,
-    //      'team_id':['a2c6322b-4c1b-4884-8f7a-a7f270de98cb'],
-    //      'service_ids':[],
-    //      'user_ids':[]
-    //     }
+       //provides incidents for the given payload
+           //params <dict> payload
+           // payload=
+           //        {
+           //            'page':1,
+           //      'status':5,
+           //      'team_id':['a2c6322b-4c1b-4884-8f7a-a7f270de98cb'],
+           //      'service_ids':[],
+           //      'user_ids':[]
+           //     }
     this.api_client.call_api(`/api/incidents/`,'get',payload)
 }
 getIncidentByNumber(incident_number)
 {
+    //returns incidents identified by incident_number
     this.api_client.call_api(`/api/incidents/${incident_number}/`,'get')
 }
 getIncidentAlerts(incident_number)
 {
-//Returns all alerts of a particular incident
-//params int incident_number: incident number of event
+       //Returns all alerts of a particular incident
+       //params int incident_number: incident number of event
  this.api_client.call_api(`/api/incidents/${incident_number}/alerts/`,'get')
 }
 getIncidentNotes(incident_number)
 {
+       //Returns IncidentNotes
     this.api_client.call_api(`/api/incidents/${incident_number}/note/`,'get')
 }
 acknowledgeOrResolveIncident(incident_number,payload)
 {
-//     payload={
-    
-//         'status':3,
-//              'incident_number':12
-// }
+   //AcknowledgesOrResolvesIncident
+       //     payload={   
+       //             'status':3,
+       //             'incident_number':12
+       //              }
 
     this.api_client.call_api(`/api/incidents/${incident_number}/`,"PATCH",payload)
 }
