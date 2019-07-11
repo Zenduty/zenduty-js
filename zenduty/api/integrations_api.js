864ce026b019c48c9633b47e77fa9a0f45fe92b1
AKASH KUMAR <33580834+freakash@users.noreply.github.com>
2019-07-11 19:06:04 +0530

    Update integrations_api.js

--- zenduty/api/integrations_api.js
+++ zenduty/api/integrations_api.js
@@ -9,25 +9,40 @@ class IntegrationsApi{
     }
 getIntegrationInService(team_id,service_id)
 {
+	 //Returns the integrations in a service
+       	//params <str> team_id: unique id of team
+	//params <str> service_id: unique id of service
     this.api_client.call_api(`/api/account/teams/${team_id}/services/${service_id}`,'get')
 }
 
 createIntegration(team_id,service_id,payload)
 {
-
-// payload={
-//     "name":"SDKNODE",
-//     "summary":"testingphase",
-//     "application":"f63f1c66-9fd9-4e54-8087-2734dda81ad8"
-// }
+	//Creates Integration in a service 
+	//params <str> team_id: unique id of team
+        //params <str> service_id: unique id of service
+	//params <dict> payload: contains the details of the new integration
+	// payload={
+	//     "name":"SDKNODE",
+	//     "summary":"testingphase",
+	//     "application":"f63f1c66-9fd9-4e54-8087-2734dda81ad8"
+	// }
     this.api_client.call_api(`/api/account/teams/${team_id}/services/${service_id}/integrations/`,'post',payload)
 }
 getIntegrationById(team_id,service_id,integration_id){
+	
+	//Returns an integration belonging to a service in a team, identified by id
+        //params <str> team_id: unique id of team
+        //params <str> service_id: unique id of service
+	//params <str> integration_id: unique id of integration
     this.api_client.call_api(`/api/account/teams/${team_id}/services/${service_id}/integrations/${integration_id}/`,'get')
 }
 
 getAlertsInIntegration(team_id,service_id,integration_id)
 {
+	//Retruns alerts in a particular integration
+        //params str team_id: unique id of team
+        //params str service_id: unique id of service
+	//params str integration_id: unique id of integration
     this.api_client.call_api(`/api/account/teams/${team_id}/services/${service_id}/integrations/${integration_id}/alerts/`,'get')
 }
 }
