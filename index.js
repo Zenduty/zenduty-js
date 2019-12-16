1a1e8b16e36150dcc5acfa8e91e72706870c871f
windynik <nikhilprem@rocketmail.com>
2019-12-16 19:11:11 +0530

    Update Fixed index file

---
+++ index.js
@@ -0,0 +1,20 @@
+//import api files
+const TeamsApi = require("./zenduty/api/teams_api.js");
+const ServicesApi = require("./zenduty/api/services_api");
+const EscalationPoliciesApi = require("./zenduty/api/escalationpolicies_api");
+const EventsApi = require("./zenduty/api/events_api");
+const IncidentsApi = require("./zenduty/api/incidents_api");
+const IntegrationsApi = require("./zenduty/api/integrations_api.js");
+const MembersApi = require("./zenduty/api/members_api");
+const SchedulesApi = require("./zenduty/api/schedules_api");
+
+module.exports = {
+  IncidentsApi,
+  IntegrationsApi,
+  MembersApi,
+  ServicesApi,
+  TeamsApi,
+  EventsApi,
+  SchedulesApi,
+  EscalationPoliciesApi
+};
